dd4fed96602d4552fb7bfbf1794c8312909d6ae0
Brunno <brunno.assis2@gmail.com>
2026-02-11 01:23:59 -0300

    chore: configure axios and cors headers

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,38 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  reactStrictMode: true,
   distDir: "dist",
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'genshin-impact.fandom.com',
-        port: '',
         pathname: '/wiki/Special:Filepath/***'
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost'
+      }
+    ]
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          { key: 'X-DNS-Prefetch-Control', value: 'on' },
+          {
+              key: 'Content-Security-Policy',
+              value: `                  
+                  connect-src 'self' 
+                  http://localhost:*
+                  https://donleta-service.discloud.app; 
+              `
+              .replace(/\s{2,}/g, ' ')
+              .trim(),
+          },
+        ],
       }
     ]
   }
